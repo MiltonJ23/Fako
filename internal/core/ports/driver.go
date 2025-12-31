ee1a620de374d69309b06d569d7ca1411164b790
MiltonJ23 <zingui.mike@ictuniversity.edu.cm>
2025-12-31 15:31:39 +0000

    Defining the contract to be implemented to talk with the outside world devices

---
+++ internal/core/ports/driver.go
@@ -0,0 +1,18 @@
+package ports
+
+import (
+	"context"
+
+	"github.com/MiltonJ23/Fako/internal/core/domain"
+)
+
+// NetworkDriver describe the behavior of any device driver . Whether its SSH, Simulation or API, will have to implement these methods
+type NetworkDriver interface {
+	// we will have to pass  the context, so that if the user press ctrl c we can stop whatever we were doing
+
+	//ApplyResource makes the actual change on  the network
+	ApplyResource(ctx *context.Context, r *domain.Resource) error
+
+	//DeleteResource revert  the change made by applyResource , Crazy right
+	DeleteResource(ctx *context.Context, r *domain.Resource) error
+}

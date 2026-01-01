4c93ca5d9cb3de56b3f7bad60d8c94dfa7a2a4f1
MiltonJ23 <zingui.mike@ictuniversity.edu.cm>
2026-01-01 03:35:55 +0000

    The domain logic of the persistence aisle of fako

---
+++ internal/core/domain/state.go
@@ -0,0 +1,21 @@
+package domain
+
+import "time"
+
+// ResourceState represents the actual state of a resource
+type ResourceState struct {
+	ID          string    `json:"id"`
+	Kind        string    `json:"kind"`
+	Status      string    `json:"status"`
+	LastApplied time.Time `json:"last_applied"`
+}
+
+type State struct {
+	Resources map[string]ResourceState `json:"resources"`
+}
+
+func NewState() *State {
+	return &State{
+		Resources: make(map[string]ResourceState),
+	}
+}

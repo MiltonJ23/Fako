b0bf65f54d2d47e024d7cbf0edff276dc1cfdaf4
MiltonJ23 <zingui.mike@ictuniversity.edu.cm>
2026-01-09 12:34:24 +0000

    Fetching the passphrase from the environment before creating a new SSH Driver

--- internal/adapters/secondary/network/factory.go
+++ internal/adapters/secondary/network/factory.go
@@ -15,13 +15,14 @@ func GetDriver(deviceType string) (ports.NetworkDriver, error) {
 		return NewLinuxDriver(), nil
 	case "ssh-target":
 		host := os.Getenv("FAKO_TARGET_HOST")
-		user := os.Getenv("FAKO_TARGET_USERNAME")
+		user := os.Getenv("FAKO_TARGET_USER")
 		keyPath := os.Getenv("FAKO_TARGET_KEY")
+		passPhrase := os.Getenv("FAKO_TARGET_PASSPHRASE")
 
 		if host == "" || user == "" || keyPath == "" {
 			return nil, fmt.Errorf("missing ssh configuration, checked the environment variables (FAKO_TARGET_*)")
 		}
-		return NewSSHDriver(host, user, keyPath)
+		return NewSSHDriver(host, user, keyPath, passPhrase)
 	default:
 		return nil, fmt.Errorf("device type %s not supported", deviceType)
 	}

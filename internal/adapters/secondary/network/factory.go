d50462586a90bd2a2f181884e9e2f2f21b83eda0
MiltonJ23 <zingui.mike@ictuniversity.edu.cm>
2026-01-15 11:36:25 +0000

    Using the domain.Secret logic to store sensitive information

--- internal/adapters/secondary/network/factory.go
+++ internal/adapters/secondary/network/factory.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/MiltonJ23/Fako/internal/core/domain"
 	"github.com/MiltonJ23/Fako/internal/core/ports"
 )
 
@@ -19,10 +20,16 @@ func GetDriver(deviceType string) (ports.NetworkDriver, error) {
 		keyPath := os.Getenv("FAKO_TARGET_KEY")
 		passPhrase := os.Getenv("FAKO_TARGET_PASSPHRASE")
 
+		var secretPass *domain.Secret
+		if passPhrase != "" {
+			secretPass = domain.NewSecret(passPhrase)
+		}
+		// After we got the passphrase from the environment, we unset the passphrase environment variable
+		os.Unsetenv("FAKO_TARGET_PASSPHRASE")
 		if host == "" || user == "" || keyPath == "" {
 			return nil, fmt.Errorf("missing ssh configuration, checked the environment variables (FAKO_TARGET_*)")
 		}
-		return NewSSHDriver(host, user, keyPath, passPhrase)
+		return NewSSHDriver(host, user, keyPath, secretPass)
 	default:
 		return nil, fmt.Errorf("device type %s not supported", deviceType)
 	}
